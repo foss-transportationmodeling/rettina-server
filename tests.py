89d8bc0f0080eb690b51863f827bf43460e9e7dd
trevphil <cheezemaker37@gmail.com>
2015-05-16 22:17:49 -0500

    edited tests

--- tests.py
+++ tests.py
@@ -17,54 +17,66 @@ class TestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         
-    def test_gtfs_data_exists(self):
+    def gtfs_data_exists(self):
         assert os.path.isfile('UC_GTFS.zip')
         zfile = zipfile.ZipFile('UC_GTFS.zip')
         zfile.extractall('tmp/GTFS/')
 
-    def test_load_agency(self):
+    def load_agency(self):
         gtfs_parser.load_agency()
         agencies = models.Agency.query.all()
         assert len(agencies) > 0
         
-    def test_load_routes(self):
+    def load_routes(self):
         gtfs_parser.load_routes()
         routes = models.Route.query.all()
         assert len(routes) > 0
 
-    def test_load_trips(self):
+    def load_trips(self):
         gtfs_parser.load_trips()
         trips = models.Trip.query.all()
         assert len(trips) > 0
         
-    def test_load_stops(self):
+    def load_stops(self):
         gtfs_parser.load_stops()
         stops = models.Stop.query.all()
         assert len(stops) > 0
         
-    def test_load_stop_times(self):
+    def load_stop_times(self):
         gtfs_parser.load_stop_times()
         stop_times = models.StopTime.query.all()
         assert len(stop_times) > 0
         
-    def test_load_calendar(self):
+    def load_calendar(self):
         gtfs_parser.load_calendar()
         calendar = models.Calendar.query.all()
         assert len(calendar) > 0
         
-    def test_load_calendar_dates(self):
+    def load_calendar_dates(self):
         gtfs_parser.load_calendar_dates()
         calendar_dates = models.CalendarDate.query.all()
         assert len(calendar_dates) > 0
         
-    def test_load_shapes(self):
+    def load_shapes(self):
         gtfs_parser.load_shapes()
         shapes = models.Shape.query.all()
         assert len(shapes) > 0
 
-    def test_remove_tmp(self):
+    def remove_tmp(self):
         shutil.rmtree('tmp/GTFS')
-        assert len([name for name in os.listdir('tmp/') if os.path.isfile(name)]) == 0
+        assert len([name for name in os.listdir('tmp/')]) == 0
+        
+    def test_load_gtfs(self):
+        self.gtfs_data_exists()
+        self.load_agency()
+        self.load_routes()
+        self.load_trips()
+        self.load_stops()
+        self.load_stop_times()
+        self.load_calendar()
+        self.load_calendar_dates()
+        self.load_shapes()
+        self.remove_tmp()
 
 if __name__ == '__main__':
     unittest.main()
