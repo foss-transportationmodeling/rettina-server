8f036b60c86f33001a5b2074e17d42c91b6ee546
trevphil <cheezemaker37@gmail.com>
2015-06-02 22:13:45 -0500

    attempt to speed up parsing

--- app/gtfs_parser.py
+++ app/gtfs_parser.py
@@ -35,22 +35,23 @@ def load_objects(file, name):
         for i, key in enumerate(keys):
             try:
                 value = values[i].strip()
-                # handle special cases for setting relationships (and DateTime)
-                if name == "StopTime" and key == "trip_id":
-                    set_trip_for_stop_time(obj, value)
-                elif name == "StopTime" and key == "stop_id":
-                    set_stop_for_stop_time(obj, value)
-                elif name == "Route" and key == "agency_id":
-                    set_agency_for_route(obj, value)
-                elif name == "Trip" and key == "route_id":
-                    set_route_for_trip(obj, value)
-                elif name == "Shape" and key == "shape_id":
-                    set_route_for_shape(obj, value)
-                else:
-                    if hasattr(obj, key):
-                            setattr(obj, key, value)
             except IndexError:
                 print "A value is missing from " + file
+                continue
+            # handle special cases for setting relationships
+            if name == "StopTime" and key == "trip_id":
+                set_trip_for_stop_time(obj, value)
+            elif name == "StopTime" and key == "stop_id":
+                set_stop_for_stop_time(obj, value)
+            elif name == "Route" and key == "agency_id":
+                set_agency_for_route(obj, value)
+            elif name == "Trip" and key == "route_id":
+                set_route_for_trip(obj, value)
+            elif name == "Shape" and key == "shape_id":
+                set_route_for_shape(obj, value)
+            else:
+                if hasattr(obj, key):
+                    setattr(obj, key, value)
         objects.append(obj)
     return objects
 
