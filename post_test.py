8dd6091f3864917ce45f03938f55ab0eabed394b
trevphil <cheezemaker37@gmail.com>
2015-07-14 17:43:57 -0500

    added test case for location POST

---
+++ post_test.py
@@ -0,0 +1,34 @@
+import requests
+
+url = 'localhost:5000/locations'
+
+payload = {
+	'trip_id' : '1 UConn Transportation Service',
+	'locations' :
+	[
+		{
+			'x' : 1,
+			'y' : 2,
+			'timestamp' : 'Nov 30 2010 9:20:37AM',
+			'location_technology' : 'Samsung Galaxy S6'
+		},
+		{
+			'x' : 4,
+			'y' : 10,
+			'timestamp' : 'Jun 1 2015 10:00:00PM',
+			'location_technology' : 'Motorola Moto X'
+		},
+		{
+			'x' : 37.1,
+			'y' : 20.2,
+			'timestamp' : 'Aug 12 2012 4:30:00AM',
+			'location_technology' : 'LG G4'
+		}
+	]
+}
+
+headers = { 'Content-Type' : 'application/json' }
+
+response = requests.post(url, data = payload, headers = headers)
+
+print response
