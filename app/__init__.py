2a934eb525abc5d87efed23f40d9bb6396952a43
trevphil <cheezemaker37@gmail.com>
2015-07-17 17:53:11 -0500

    reverting to standard config

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config.from_object('config')
-
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:1234@localhost/app'
-app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(basedir, 'db_repository')
-
+app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
