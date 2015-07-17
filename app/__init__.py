5560d79c76c1d6c16ca7c94c2568ded4e7f23323
trevphil <cheezemaker37@gmail.com>
2015-07-17 17:36:40 -0500

    still trying to connect to MySQL

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:1234@localhost/app'
 db = SQLAlchemy(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:1234@localhost/app'
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
