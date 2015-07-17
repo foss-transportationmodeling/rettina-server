34778e6b5e44ba899a9feff2930a2081b13aec88
trevphil <cheezemaker37@gmail.com>
2015-07-17 17:02:08 -0500

    trying MySQL database URI

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://urbansystems:1234@' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@' + os.path.join(basedir, 'app.db')
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
